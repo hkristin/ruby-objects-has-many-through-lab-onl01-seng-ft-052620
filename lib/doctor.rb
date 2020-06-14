79be84b312309b052769d5e927b4ef34dcbe7406
Kristin Howard <hkristin@andrew.cmu.edu>
2020-06-14 18:02:46 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,23 @@
+class Doctor 
+  attr_accessor :name
+  
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.collect do |appointment|
+      appointment
+    end
+  end
+  
+  def new_appointment(date, patient)
+   new_appointment = Appointment.new(date, patient, self)
+ end
+end
---
+++ lib/patient.rb
@@ -0,0 +1,23 @@
+class Patient
+  attr_accessor :name
+  
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_appointment(date,doctor )
+    
+  end
+  def appointments
+  end
+
+  def doctors
+  
+  end
+end
